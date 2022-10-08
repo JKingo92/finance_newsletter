899e2c322cdd8cea581aeb2c683eb252f47c8778
JKingo92 <jonathan.takada@gmail.com>
2022-10-08 19:36:55 -0300

    async changes

--- newsletter.py
+++ newsletter.py
@@ -2,110 +2,134 @@
 import os
 import re
 import json
+import asyncio
 from utils import get_soup_from_url, extract_links, telegram_bot_sendtext
 
 
-class NewsletterScrapper():
-    '''
-    Scraper that sends a json newsletter
-    :list keywords: list of keywords to be scrapped on available sites
+# Moneytimes
+async def parse_moneytimes(soup, keywords):
+    """Scrape moneytimes site"""
+    result = {}
+    top_news = soup.find("ol")
+    top_news = extract_links(top_news)
 
-    '''
-    def __init__(self, keywords):
-        self.keywords = keywords
+    other_news = soup.findAll("h2", {"class": "news-item__title"})
+    links_list = [extract_links(news)[0] for news in other_news]
 
-    # Moneytimes
-    def scrap_moneytimes(self):
-        '''Scrape moneytimes site'''
-        result = {}
-        soup = get_soup_from_url(os.environ.get('MONEYTIMES_URL', 'https://www.moneytimes.com.br/'))
-        top_news = soup.find('ol')
-        top_news = extract_links(top_news)
-        
-        other_news = soup.findAll('h2', {'class': 'news-item__title'})
-        links_list = [extract_links(news)[0] for news in other_news]
+    all_links = top_news + links_list
+    all_links = list(set(all_links))  # Remove duplicates
 
-        all_links = top_news + links_list
-        all_links = list(set(all_links))  # Remove duplicates
+    for link in all_links:
+        try:
+            print("Reading {} ...".format(link))
+            soup = await get_soup_from_url(link)
+            soup = soup[0]
+            search = soup.findAll("strong")
+            for word in search:
+                word = word.getText()
+                for kwords in keywords:
+                    if bool(re.search(kwords, word, re.IGNORECASE)):
+                        if link in result:
+                            result[link].append(kwords)
+                        else:
+                            result[link] = [kwords]
+        except Exception as ex:
+            print(f"Something went wrong: {ex}")
+    return result
 
-        for link in all_links:
-            try:
-                print('Reading {} ...'.format(link))
-                soup = get_soup_from_url(link)
-                search = soup.findAll('strong')
-                for word in search:
-                    word = word.getText()
-                    for kwords in self.keywords:
-                        if bool(re.search(kwords, word, re.IGNORECASE)):
+
+async def parse_investing(soup, keywords):
+    """Scrape investing site"""
+    result = {}
+    investing_url = os.environ.get("INVESTING_URL", "https://br.investing.com/")
+    all_news = soup.findAll("div", {"class": "textDiv"})
+
+    links_investing = [extract_links(news) for news in all_news]
+    links_investing = [investing_url + x[0] for x in links_investing if len(x) > 0]
+    for link in links_investing:
+        try:
+            print("Reading {} ...".format(link))
+            soup = await get_soup_from_url(link)
+            soup = soup[0]
+            src = soup.find("span", {"class": "aqPopupWrapper js-hover-me-wrapper"})
+            if src:
+                for word in keywords:
+                    if src.find(text=word):
+                        if link in result:
+                            result[link].append(word)
+                        else:
+                            result[link] = [word]
+        except Exception as ex:
+            print(f"Something went wrong: {ex}")
+    return result
+
+
+async def parse_infomoney(soup, keywords):
+    """Scrape infomoney site"""
+    result = {}
+    src = soup.findAll("div", {"id": "infiniteScroll"}, limit=30)
+    news = [extract_links(item) for item in src][0]
+    news = list(set(news))  # Remove duplicates
+
+    for link in news:
+        try:
+            print("Reading {} ...".format(link))
+            soup = await get_soup_from_url(link)
+            soup = soup[0]
+            src = soup.findAll("p")
+            for item in src:
+                if item.a:
+                    for kword in keywords:
+                        if item.find(text=kword):
                             if link in result:
-                                result[link].append(kwords)
+                                result[link].append(kword)
                             else:
-                                result[link] = [kwords]
-            except Exception as ex:
-                print(f'Something went wrong: {ex}')
-        return result
-
-    def scrap_investing(self):
-        '''Scrape investing site'''
-        result = {}
-        investing_url = os.environ.get('INVESTING_URL', 'https://br.investing.com/')
-        soup = get_soup_from_url(investing_url)
-        all_news = soup.findAll('div', {'class': 'textDiv'})
-        
-        links_investing = [extract_links(news) for news in all_news]
-        links_investing = [investing_url + x[0] for x in links_investing if len(x) > 0]
-        for link in links_investing:
-            try:
-                print('Reading {} ...'.format(link))
-                soup = get_soup_from_url(link)
-                src = soup.find('span', {'class': 'aqPopupWrapper js-hover-me-wrapper'})
-                if src:
-                    for word in self.keywords:
-                        if src.find(text=word):
-                            if link in result:
-                                result[link].append(word)
-                            else:
-                                result[link] = [word]
-            except Exception as ex:
-                print(f'Something went wrong: {ex}')
-        return result
-
-    def scrap_infomoney(self):
-        '''Scrape infomoney site'''
-        result = {}
-        soup = get_soup_from_url(os.environ.get('INFOMONEY_URL', 'https://www.infomoney.com.br/ultimas-noticias/'))
-        src = soup.findAll('div', {'id': "infiniteScroll"}, limit=30)
-        news = [extract_links(item) for item in src][0]
-        news = list(set(news))  # Remove duplicates
-
-        for link in news:
-            try:
-                print('Reading {} ...'.format(link))
-                soup = get_soup_from_url(link)
-                src = soup.findAll('p')
-                for item in src:
-                    if item.a:
-                        for kword in self.keywords:
-                            if item.find(text=kword):
-                                if link in result:
-                                    result[link].append(kword)
-                                else:
-                                    result[link] = [kword]
-            except Exception as ex:
-                print(f'Something went wrong: {ex}')
-        return result
+                                result[link] = [kword]
+        except Exception as ex:
+            print(f"Something went wrong: {ex}")
+    return result
 
 
-if __name__ == '__main__':
-    with open('keywords.txt', 'r') as file:
+async def main():
+    with open("keywords.txt", "r") as file:
         file_content = file.read()
-        file_content = file_content.split('\n')
+        keywords = file_content.split("\n")
 
-        newsletter = NewsletterScrapper(file_content)
-        result = {**newsletter.scrap_moneytimes(), **newsletter.scrap_investing(), **newsletter.scrap_infomoney()}
+        # newsletter = NewsletterScrapper(file_content)
+        sites = {
+            "moneytimes": os.environ.get(
+                "MONEYTIMES_URL", "https://www.moneytimes.com.br/"
+            ),
+            "investing": os.environ.get("INVESTING_URL", "https://br.investing.com/"),
+            "infomoney": os.environ.get(
+                "INFOMONEY_URL", "https://www.infomoney.com.br/ultimas-noticias/"
+            ),
+        }
+
+        tasks = []
+        tasks.append(asyncio.create_task(get_soup_from_url(sites["moneytimes"])))
+        tasks.append(asyncio.create_task(get_soup_from_url(sites["investing"])))
+        tasks.append(asyncio.create_task(get_soup_from_url(sites["infomoney"])))
+        raw_sites = await asyncio.gather(*tasks)
+
+        tasks = []
+        for site in raw_sites:
+            if site[1] == "https://www.moneytimes.com.br/":
+                tasks.append(asyncio.create_task(parse_moneytimes(site[0], keywords)))
+            elif site[1] == "https://br.investing.com/":
+                tasks.append(asyncio.create_task(parse_investing(site[0], keywords)))
+            else:
+                tasks.append(asyncio.create_task(parse_infomoney(site[0], keywords)))
+
+        result = await asyncio.gather(*tasks)
+        print(result)
 
         # Send message to bot
         telegram_bot_sendtext(result)
         # Write json file
-        with open('news.json', 'w') as jsonresult:
+        with open("news.json", "w") as jsonresult:
             json.dump(result, jsonresult)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- utils.py
+++ utils.py
@@ -1,37 +1,21 @@
 import os
 import requests
+import aiohttp
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 from typing import List, Union, Dict
 
 
-def get_soup_from_url(url: str) -> Union[BeautifulSoup, None]:
-    """
-    From given url it returns a BeautifulSoup object or empty in case of failure.
-
-    Args:
-        :str url: URL to be scrapped.
-
-    Returns:
-        Beautiful soup object of the URL page or empty string in case of failure.
-
-    Raises:
-        HTTPError: Log the http error code
-        URLError:  Log the reason of URL error.
-    """
-    soup = ''
-    try:
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
-        req = Request(url, headers=headers)
-        response = urlopen(req)
-        html = response.read()
-        soup = BeautifulSoup(html, 'html.parser')
-    except HTTPError as httperror:
-        print(f'HTTP error in {html}. Error code:', httperror.code)
-    except URLError as urlerror:
-        print(f'URL error in {html}. Reason: ', urlerror.reason)
-    return soup
+async def get_soup_from_url(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers) as response:
+            html = await response.text()
+            soup = BeautifulSoup(html, "html.parser")
+            return soup, url
 
 
 def extract_links(soup: BeautifulSoup) -> List[str]:
@@ -45,8 +29,8 @@ def extract_links(soup: BeautifulSoup) -> List[str]:
         List of URLs page.
     """
     links = []
-    for link in soup.find_all('a', href=True):
-        links.append(link['href'])
+    for link in soup.find_all("a", href=True):
+        links.append(link["href"])
     return links
 
 
@@ -60,9 +44,14 @@ def telegram_bot_sendtext(bot_message: str) -> None:
     Returns:
         None.
     """
-    bot_token = os.environ.get('TELEGRAM_BOT_TOKEN', '')
-    bot_chatID = os.environ.get('TELEGRAM_BOT_CHAT', '')
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text='
-    send_text = send_text + '{}'.format(bot_message)
+    bot_token = os.environ.get("TELEGRAM_BOT_TOKEN", "")
+    bot_chatID = os.environ.get("TELEGRAM_BOT_CHAT", "")
+    send_text = (
+        "https://api.telegram.org/bot"
+        + bot_token
+        + "/sendMessage?chat_id="
+        + bot_chatID
+        + "&parse_mode=Markdown&text="
+    )
+    send_text = send_text + "{}".format(bot_message)
     requests.get(send_text)
-
